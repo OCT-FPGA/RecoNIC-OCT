0131905024102413a4820ee67670e41dbe906791
Yicheng Qian <qian.yich@northeastern.edu>
2025-09-04 19:42:21 +0000

    update script for driver update automation

--- scripts/gen_nic_driver.sh
+++ scripts/gen_nic_driver.sh
@@ -16,6 +16,12 @@ drv_patch=${root_dir}/patches/open-nic-driver/onic.patch
 cd ${drv_dir}
 git apply --whitespace=fix ${drv_patch}
 
+rm -rf libqdma
+cp -r ${root_dir}/drivers/ernic4_driver_update/libqdma_for_qdma5/ libqdma
+cp ${root_dir}/drivers/ernic4_driver_update/qdma_device.h libqdma/
+
+echo -e "update libqdma for QDMA 5.0"
+
 echo -e "gen_nic_driver.sh done!"
 
 cd ${cur_dir}
